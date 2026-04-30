a16e04dd511dc58e0c5b3707bd935e576e03ce2e
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2026-04-30 23:27:06 +0500

    Update Spread_beeper.py

--- Spread_beeper.py
+++ Spread_beeper.py
@@ -1,57 +1,55 @@
 from karel.stanfordkarel import *
 
 """
-Each row starts in front of a stack of beepers. Karel should pick them
-up, one at a time, and spread them down the row. 
-Caution! Karel can't count, and starts with infinite beepers in
-her bag. How can you solve this puzzle?
+Each row starts with a stack of beepers.
+Karel should pick them up, one at a time, and spread them down the row.
+
+Caution! Karel can't count, and starts with infinite beepers in her bag.
 """
 
-"""
-move - 
-spread:
-remove beeper--pick beeper move-put-beeper
-step_back
-"""
 def main():
     move()
-    spread()
-    step_back() 
+    spread_beeper()
+    step_back()
 
-def spread():
+
+def spread_beeper():
     while beepers_present():
         pick_beeper()
         if beepers_present():
-            move_to_end() 
+            move_to_end()
             put_beeper()
-            reset() 
-    put_beeper()
+            reset()
+            put_beeper()
+
+
 def move_to_end():
     while beepers_present():
         move()
+
+
 def reset():
-    turn_around()
+    turn_around()  # facing west
     move_to_wall()
-    turn_around()
-    move()
+    turn_around()  # facing east
+    move()  # 2nd column
+
+
 def move_to_wall():
     while front_is_clear():
         move()
+
+
 def turn_around():
     turn_left()
     turn_left()
+
+
 def step_back():
     turn_around()
     move()
     turn_around()
 
-
-        
-    
-
-
-
-    
 
 # There is no need to edit code beyond this point
 if __name__ == '__main__':
